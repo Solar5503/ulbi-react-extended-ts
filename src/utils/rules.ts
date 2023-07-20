ae707ad19ca7501a614789eae4c69081fa168b37
Vadim <76094360+Solar5503@users.noreply.github.com>
2023-07-20 23:55:35 +0300

    fixed rule is date after (#4)

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,4 +1,4 @@
-import type { Dayjs } from 'dayjs'
+import daysjs, { Dayjs } from 'dayjs'
 export const rules = {
   required: (message = 'Required field!') => ({
     required: true,
@@ -6,10 +6,11 @@ export const rules = {
   }),
   isDateAfter: (message: string) => () => ({
     validator(_: unknown, value: Dayjs) {
-      const now = new Date().toLocaleDateString()
-      const dateEvent = value?.format('DD.MM.YYYY')
-
-      if (now <= dateEvent) return Promise.resolve()
+      if (
+        daysjs().isBefore(daysjs(value)) ||
+        daysjs().isSame(daysjs(value), 'day')
+      )
+        return Promise.resolve()
       return Promise.reject(new Error(message))
     },
   }),
